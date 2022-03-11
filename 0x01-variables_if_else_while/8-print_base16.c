7d456b641f129736fabc3348d71a46b2bdd85206
Cliff042 <cliff.o.owino@gmail.com>
2022-03-12 02:09:58 +0300

    0x01. C - Variables, if, else, while

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all the numbers of base 16 in lowercase.
+(*
+ * Return: 0 if success.
+ */
+ 
+ int main(void)
+ {
+	int i;
+	char c; 
+				
+	for (i = '0'; i <= '9'; i++)
+	{	
+		putchar(i);
+	}
+	for (c = 'a'; c <= 'f'; c++)
+	{
+		putchar(c);
+	}
+		 
+	putchar('\n');
+	return (0);
+}
