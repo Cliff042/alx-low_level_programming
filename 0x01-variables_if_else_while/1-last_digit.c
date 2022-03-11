59e8f73682c35309644644777052f64781c2ccff
Cliff042 <cliff.o.owino@gmail.com>
2022-03-11 20:18:35 +0300

    0x01. C - Variables, if, else, while

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 
 /**
- * main - Determine if last digit of a random number is greater than
+ * main - Determine if last digit of a random number is greater than 
  * less than 5 or is zero.  
  * Return: Always 0 if successful.
  */
