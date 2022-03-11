c0c42c33abf3909727c1b84cd05e670e001803ca
Cliff042 <cliff.o.owino@gmail.com>
2022-03-12 02:15:04 +0300

    0x01. C - Variables, if, else, while

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -5,21 +5,21 @@
 (*
  * Return: 0 if success.
  */
- 
- int main(void)
- {
+
+int main(void)
+{
 	int i;
-	char c; 
-				
+	char c;
+	
 	for (i = '0'; i <= '9'; i++)
-	{	
+	{
 		putchar(i);
 	}
 	for (c = 'a'; c <= 'f'; c++)
 	{
 		putchar(c);
 	}
-		 
+	
 	putchar('\n');
 	return (0);
 }
