cef9a34fdf807b7d9b29b3e6cc2ad9b13c6fe43b
Cliff042 <cliff.o.owino@gmail.com>
2022-03-11 15:04:23 +0300

    0x00. C - Hello, World

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,10 +3,10 @@
 /*
  * main - Prints out a string to stdout.
  * Return: Always 0 (Success).
- */
+*/
 
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return 0;
+	return(0);
 }
